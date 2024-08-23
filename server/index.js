a62186c75526e2d28936e3e42921f3a97ef5bfd6
opeyemi <opeyemiodunayo2018@gmail.com>
2024-08-23 04:03:22 -0700

    create and run the server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
+dotenv.config()
 
 let app = express()
+app.use(express.json())
+app.use(cookieParser())
 
-app.listen(3000, ()=> {
-    console.log('server is running on  port 3000');
-    
-})
+mongoose.connect(process.env.mongoUrl)
+.then(() => {
+    app.listen(3000, ()=> {
+        console.log('server is running is connected');
+        
+    })
+}).catch(err => console.log(err))
+
+
