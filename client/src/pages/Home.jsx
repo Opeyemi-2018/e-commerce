bda61c7941503ab54fe1cdd6f4ecf9c3a0b1c892
opeyemi <opeyemiodunayo2018@gmail.com>
2024-10-01 04:21:43 -0700

    some useful changes made

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
           background: `linear-gradient(to bottom, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0.8) 60%, rgba(0, 0, 0, 0.3) 90%), url(${laptop}) center/cover no-repeat`,
         }}
       >
-        <div className="w-full py-6 hidden sm:block px-3 ">
+        <div className="top-home-div w-full py-6  px-3 md:inline hidden ">
           <div className="max-w-6xl mx-auto flex md:gap-10 gap-2 justify-center ">
             <div className="bg-white md:px-10 px-4 rounded-sm">
               <div className="flex flex-col gap-4 my-4">
@@ -113,41 +113,44 @@ const Home = () => {
         </div>
 
         {/* Landing content for mobile */}
+        <div className="sm:hidden inline">
+          <div className=" relative w-full py-8 flex items-center justify-center">
+            {/* Content Wrapper */}
 
-        <div className="sm:hidden relative w-full py-8 flex items-center justify-center">
-          {/* Content Wrapper */}
-
-          <div className="py-2 flex items-center flex-col justify-center z-20">
-            <p className="text-1xl py-3 text-[15px] space-x-10 text-gray-500">
-              NEW PRODUCT
-            </p>
-            <h1 className="md:text-5xl text-white text-2xl font-extrabold">
-              XX99 MARK II
-            </h1>
-            <h1 className="md:text-5xl text-white text-2xl font-extrabold">
-              GADGETS
-            </h1>
-            <div className="py-4 flex items-center flex-col">
-              <p className="text-white">shop with us today</p>
-              <ReactTyped
-                className="text-[#ffa45c]"
-                strings={["For Exclusive deal", "and Enjoy quality products"]}
-                typeSpeed={100}
-                backSpeed={20}
-                loop
-              />
+            <div className="py-2 flex items-center flex-col justify-center z-20">
+              <p className="text-1xl py-3 text-[15px] space-x-10 text-gray-500">
+                NEW PRODUCT
+              </p>
+              <h1 className="md:text-5xl text-white text-2xl font-extrabold">
+                XX99 MARK II
+              </h1>
+              <h1 className="md:text-5xl text-white text-2xl font-extrabold">
+                GADGETS
+              </h1>
+              <div className="py-4 flex items-center flex-col">
+                <p className="text-white">shop with us today</p>
+                <ReactTyped
+                  className="text-[#ffa45c]"
+                  strings={["For Exclusive deal", "and Enjoy quality products"]}
+                  typeSpeed={100}
+                  backSpeed={20}
+                  loop
+                />
+              </div>
+              <Link className="bg-[#ffa45c] text-white px-2 py-1">
+                SEE PRODUCT
+              </Link>
             </div>
-            <Link className="bg-[#ffa45c] text-white px-2 py-1">
-              SEE PRODUCT
-            </Link>
-          </div>
 
-          {/* Background Image */}
-          <div className="absolute inset-0 flex items-center justify-center z-10">
-            <img src={HeadSet} alt="Headset" className=" md:animate-spin" />
+            {/* Background Image */}
+            <div className="absolute inset-0 flex items-center justify-center z-10">
+              <img src={HeadSet} alt="Headset" className=" " />
+            </div>
           </div>
         </div>
       </div>
+
+      {/* product div */}
       <div className="px-3 mt-6 mb-10">
         <div className="max-w-6xl grid md:grid-cols-4 grid-cols-2 mx-auto gap-4">
           {products.map((product) => {
